56a459b061ad4b710b033664f4b679c13b175afb
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-08-14 22:16:13 -0400

    creando archivo

---
+++ aritmetica.py
@@ -0,0 +1,2 @@
+def raizCuadrada (y):
+    return y ** 0.5
---
+++ ejercicio.py
@@ -0,0 +1,11 @@
+#invocando funcion
+from aritmetica import raizCuadrada
+
+# variable
+y = 81
+
+#imprimiendo resultado
+print ("La raiz cuadrada de: ", y, " es : ", int(raizCuadrada(y)))
+
+# Evaluacion modulo 3 sesion 3
+# Thelma Delgado
